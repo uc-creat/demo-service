f9497fc3da35ad2b0387e56ab75b8fd9767a9b85
Sunil Kumar <sunil.kumar@thoughtworks.com>
2022-02-16 19:59:25 +0530

    use docker multi-stage build feature for gradle build and building service image

--- docker/app/build.sh
+++ docker/app/build.sh
@@ -4,12 +4,5 @@ set -e
 cd ../..
 echo "working directory ${PWD}"
 
-echo "building executable jar"
-./gradlew clean bootJar
-
-mkdir -p build/dependency && (cd build/dependency; jar -xf ../libs/*.jar)
-
 echo "building docker image"
-
 docker build -t kalavithi-service -f ./docker/app/Dockerfile .
-
