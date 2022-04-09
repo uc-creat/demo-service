cf9da9897457878bcbbc87cecd3f092ac537b211
Sunil Kumar <sunil.kumar@thoughtworks.com>
2022-04-09 17:54:28 +0530

    add @Repository annotation

--- src/main/java/com/tw/prograd/image/ImageRepository.java
+++ src/main/java/com/tw/prograd/image/ImageRepository.java
@@ -1,6 +1,8 @@
 package com.tw.prograd.image;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<ImageEntity, Integer> {
 }
