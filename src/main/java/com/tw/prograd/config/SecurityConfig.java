46451b4b01c634904b76f43695221d402bc1c110
Sunil Kumar <sunil.kumar@thoughtworks.com>
2022-04-10 09:04:57 +0530

    disable auth for GET images routes

--- src/main/java/com/tw/prograd/config/SecurityConfig.java
+++ src/main/java/com/tw/prograd/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(GET, "/images").permitAll()
+                .antMatchers(GET, "/images/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
